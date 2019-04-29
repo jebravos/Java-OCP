3a89957fe182bc0859492223afe7a28c1cf22be8
esteban <esbravo-ext@pcis.lvmh-pc.com>
2019-04-29 15:49:17 +0200

    Comparator in TreeSet

--- src/collections_and_generics/TestingTreeSet.java
+++ src/collections_and_generics/TestingTreeSet.java
@@ -1,7 +1,5 @@
 package collections_and_generics;
 
-import com.sun.corba.se.impl.orb.ParserTable;
-
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -16,7 +14,7 @@ public class TestingTreeSet {
 
     }
 
-    public void testingTreeSet(){
+    public void testingTreeSet() {
 
         // TreeSet implements NavigableSet interface which extends from SortedSet
         // TreeSet uses a TreeMAp to store data
@@ -47,9 +45,9 @@ public class TestingTreeSet {
         System.out.println("last " + treeSet.last());
 
         // TreeSet cannot contain null values. It will throw a NullPointerException
-        try{
+        try {
             treeSet.add(null);
-        } catch(NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println("TreeSet cannot contain null values. It will throw a NullPointerException");
         }
 
@@ -58,9 +56,19 @@ public class TestingTreeSet {
         System.out.println("-------------------------------------------------------------");
 
         // If we use a collection to create a new TreeSet instance, the result set will be sorted according to the natural order of its elements
-        List<Integer> numbers = Arrays.asList(3,2,5,1,3,8,7);
+        List<Integer> numbers = Arrays.asList(3, 2, 5, 1, 3, 8, 7);
         TreeSet<Integer> numberTree = new TreeSet<>(numbers);
         System.out.println(numberTree);
+
+        // Objects in a TreeSet should be comparable. They must implement Comparable or whe can define a comparator in the TreeSet constructor
+        // TreeSet<Elephant> elephants = new TreeSet<>(); // This will compile but a ClassCastException is thrown in elephants.add
+        TreeSet<Elephant> elephants = new TreeSet<>(Comparator.comparing(Elephant::getName));
+        elephants.add(new Elephant("carlos"));
+        elephants.add(new Elephant("alberto"));
+        elephants.add(new Elephant("benito"));
+
+        System.out.println(elephants);
+
 
     }
 
--- src/collections_and_generics/TestsGenerics.java
+++ src/collections_and_generics/TestsGenerics.java
@@ -57,6 +57,11 @@ class Elephant {
     public String getName() {
         return name;
     }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
 
 class Giraffe {
