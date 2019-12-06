0ab35bf84f540b5bdf5fb3dd1ba07154acf0a984
esteban_bravo <esteban_bravo@groupvsc.com>
2019-12-06 16:11:19 +0100

    Operator ++

---
+++ src/main/java/com/bravo/ocp/io/fundamentals/OperatorPlusPlus.java
@@ -0,0 +1,17 @@
+package com.bravo.ocp.io.fundamentals;
+
+import static com.bravo.ocp.utils.PrintUtils.println;
+
+public class OperatorPlusPlus {
+
+
+  public static void main(String[] args) {
+    int i = 0;
+    // print first then increment
+    println(i++);
+    println(i);
+
+    println(i + 1);
+  }
+
+}
